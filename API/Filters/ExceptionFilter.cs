57500d718cceee10f2fffc09d06218d8858b7fca
Steven <stevelee2497@gmail.com>
2024-03-04 14:42:23 +0700

    Feature/Exception Filter (#7)
    * Clean up smell code
    
    * Added Exception Filter
    
    * Updated logic logging
    
    ---------
    
    Co-authored-by: Quoc Tran <quoc.tran@saigontechnology.com>

---
+++ API/Filters/ExceptionFilter.cs
@@ -0,0 +1,41 @@
+﻿using API.Models;
+using Core.Exceptions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace API.Filters
+{
+    public class ExceptionFilter : IExceptionFilter
+    {
+        private readonly ILogger<ExceptionFilter> _logger;
+        private readonly IHostEnvironment _hostEnvironment;
+
+        public ExceptionFilter(IHostEnvironment hostEnvironment, ILogger<ExceptionFilter> logger)
+        {
+            _logger = logger;
+            _hostEnvironment = hostEnvironment;
+        }
+
+        public void OnException(ExceptionContext context)
+        {
+            _logger.LogError(context.Exception, "Error on {env} at {time}: {message}", _hostEnvironment.EnvironmentName, DateTime.Now, context.Exception.Message);
+
+            switch (context.Exception)
+            {
+                case SystemApiException exception:
+                    context.Result = new JsonResult(new SystemApiErrorModel { Code = exception.Code, Message = exception.Message, StackTrace = exception.StackTrace })
+                    {
+                        StatusCode = exception.StatusCode
+                    };
+                    break;
+
+                default:
+                    context.Result = new JsonResult(new SystemApiErrorModel { Code = "GENERIC CODE", Message = context.Exception.Message, StackTrace = context.Exception.StackTrace })
+                    {
+                        StatusCode = StatusCodes.Status500InternalServerError
+                    };
+                    break;
+            }
+        }
+    }
+}
---
+++ Core/Exceptions/SystemApiException.cs
@@ -0,0 +1,17 @@
+﻿using System.Net;
+
+namespace Core.Exceptions
+{
+    public class SystemApiException : Exception
+    {
+        public int StatusCode { get; set; }
+
+        public string Code { get; set; }
+
+        public SystemApiException(string message, string code = "GENERIC CODE", int statusCode = (int)HttpStatusCode.InternalServerError) : base(message)
+        {
+            Code = code;
+            StatusCode = statusCode;
+        }
+    }
+}
