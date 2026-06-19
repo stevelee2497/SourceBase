b14e7486b919dbe2891fc260f51de3e768fd926a
Quoc Tran <stevelee2497@gmail.com>
2026-06-19 15:14:21 +0700

    feat: add Aspire.Hosting.NodeJs package and update Wallets page styles

--- SourceBase.AppHost/Program.cs
+++ SourceBase.AppHost/Program.cs
@@ -4,4 +4,6 @@ var api = builder.AddProject<Projects.SourceBase_Api>("sourcebase-api");
 
 builder.AddProject<Projects.SourceBase_Web>("sourcebase-web").WithReference(api).WaitFor(api);
 
+builder.AddNpmApp("tailwind-watch", "../SourceBase.Web", "watch:css");
+
 builder.Build().Run();
