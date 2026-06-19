7052c0510320a479db8a978e5a7650801bce2ae2
Quoc Tran <stevelee2497@gmail.com>
2026-06-19 14:47:51 +0700

    feat: enhance response compression and configure SignalR options

--- SourceBase.Web/Program.cs
+++ SourceBase.Web/Program.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Server;
+using Microsoft.AspNetCore.ResponseCompression;
 using Serilog;
 using SourceBase.Web;
 using SourceBase.Web.Auth;
@@ -13,7 +15,32 @@ builder.Host.UseSerilog((ctx, cfg) => cfg.ReadFrom.Configuration(ctx.Configurati
 builder.AddServiceDefaults();
 
 
+builder.Services.AddResponseCompression(opts =>
+{
+    opts.EnableForHttps = true;
+    opts.Providers.Add<BrotliCompressionProvider>();
+    opts.Providers.Add<GzipCompressionProvider>();
+});
+builder.Services.Configure<BrotliCompressionProviderOptions>(opts =>
+    opts.Level = System.IO.Compression.CompressionLevel.Fastest);
+builder.Services.Configure<GzipCompressionProviderOptions>(opts =>
+    opts.Level = System.IO.Compression.CompressionLevel.Fastest);
+
 builder.Services.AddRazorComponents().AddInteractiveServerComponents();
+
+builder.Services.Configure<CircuitOptions>(options =>
+{
+    options.DisconnectedCircuitRetentionPeriod = TimeSpan.FromMinutes(5);
+    options.DisconnectedCircuitMaxRetained = 200;
+    options.JSInteropDefaultCallTimeout = TimeSpan.FromMinutes(1);
+});
+
+builder.Services.AddSignalR(options =>
+{
+    options.KeepAliveInterval = TimeSpan.FromSeconds(15);
+    options.ClientTimeoutInterval = TimeSpan.FromSeconds(60);
+    options.HandshakeTimeout = TimeSpan.FromSeconds(30);
+});
 
 builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddAuthorizationCore();
@@ -49,9 +76,15 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error");
     app.UseHsts();
     app.UseHttpsRedirection();
+    app.UseResponseCompression();
 }
-
-app.UseStaticFiles();
+app.UseStaticFiles(new StaticFileOptions
+{
+    OnPrepareResponse = ctx =>
+    {
+        ctx.Context.Response.Headers.CacheControl = "public, max-age=3600";
+    }
+});
 app.UseAntiforgery();
 
 app.MapDefaultEndpoints();
